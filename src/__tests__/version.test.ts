c9f23b839a4203736aa7d455c58ed97ab32af2c5
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-12-26 22:35:35 +0100

    Add tests for version.ts

---
+++ src/__tests__/version.test.ts
@@ -0,0 +1,100 @@
+// Action to build any Python version on the latest labels and install it into the local tool cache.
+// Copyright (C) 2022 Matteo Dell'Acqua
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import * as manifestTC from '@actions/tool-cache/lib/manifest';
+import * as tc from '@actions/tool-cache';
+import {PyPyTest, SetupPythonTests, manifestUrl} from './version.fixtures';
+import {SetupPythonResult, getSetupPythonResult, isPyPy} from '../version';
+import {beforeAll, describe, expect, jest, test} from '@jest/globals';
+import axios from 'axios';
+
+jest.mock('@actions/core');
+jest.mock('@actions/tool-cache');
+jest.mock('@actions/tool-cache/lib/manifest');
+
+const mockedTC = jest.mocked(tc);
+const originalModule: typeof tc = jest.requireActual('@actions/tool-cache');
+mockedTC.findFromManifest.mockImplementation(
+  async (versionSpec, stable, manifest, archFilter) => {
+    return originalModule.findFromManifest(
+      versionSpec,
+      stable,
+      manifest,
+      archFilter
+    );
+  }
+);
+
+const mockedManifestTC = jest.mocked(manifestTC);
+const originalManifest: typeof manifestTC = jest.requireActual(
+  '@actions/tool-cache/lib/manifest'
+);
+mockedManifestTC._getOsVersion.mockImplementation(() => {
+  if (process.platform === 'linux') {
+    return '22.04';
+  } else {
+    return originalManifest._getOsVersion();
+  }
+});
+mockedManifestTC._findMatch.mockImplementation(
+  async (versionSpec, stable, candidates, archFilter) => {
+    return originalManifest._findMatch(
+      versionSpec,
+      stable,
+      candidates,
+      archFilter
+    );
+  }
+);
+
+describe('Is PyPy', () => {
+  test.each(PyPyTest)(
+    'returns $expectedPyPy with python version $pythonVersion.type-$pythonVersion.version',
+    ({pythonVersion, expectedPyPy}) => {
+      expect(isPyPy(pythonVersion)).toBe(expectedPyPy);
+    }
+  );
+});
+
+describe('getSetupPythonResult', () => {
+  let manifest: tc.IToolRelease[];
+
+  beforeAll(async () => {
+    const response = await axios.get(manifestUrl);
+    manifest = response.data;
+    mockedTC.getManifestFromRepo.mockResolvedValue(manifest);
+  });
+
+  test.each(SetupPythonTests)(
+    `returns $expectedResult.${process.platform} for input version $inputs.version.type-$inputs.version.version and architecture $inputs.architecture`,
+    async ({expectedResult, inputs}) => {
+      let platformResult: SetupPythonResult;
+      if (process.platform === 'linux') {
+        platformResult = expectedResult.linux;
+      } else if (process.platform === 'darwin') {
+        platformResult = expectedResult.darwin;
+      } else if (process.platform === 'win32') {
+        platformResult = expectedResult.win32;
+      } else {
+        throw new Error(`Action not supported on ${process.platform}`);
+      }
+
+      const result = await getSetupPythonResult(inputs);
+
+      expect(result).toEqual(platformResult);
+    }
+  );
+});
