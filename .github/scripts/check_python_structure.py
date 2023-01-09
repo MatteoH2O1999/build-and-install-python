0f929d8dcca39bf4d9dee1baf71a73bd1498c661
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2023-01-10 00:05:50 +0100

    Add check for installed python folder structure

---
+++ .github/scripts/check_python_structure.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import pip
+import numpy
+
+print("executable: " + sys.executable)
+print("os location: " + os.__file__)
+print("pip location: " + pip.__file__)
+print("numpy location: " + numpy.__file__)
