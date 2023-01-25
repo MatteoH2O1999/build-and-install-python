c50e32c0e33fbf97eef457568a0887a34a023d36
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2023-01-25 04:25:15 +0100

    Update setup pip script

--- .github/scripts/setup_pip.ps1
+++ .github/scripts/setup_pip.ps1
@@ -1,21 +1,19 @@
 $Version = $env:PYTHON_VERSION
 $Semver = $Version.Split('.')
-$pip = 0
-if ([int]$Semver[0] -eq 3 -and [int]$Semver[1] -ge 4) {
-    Invoke-Expression -Command "python -m ensurepip"
-    if ([int]$Semver[1] -gt 4) {
-        Invoke-Expression -Command "python -m pip install --upgrade pip"
-    }
-    $pip = 1
-} elseif ([int]$Semver[1] -ge 3) {
-    $GetPipFile = New-TemporaryFile
-    Invoke-WebRequest -Uri 'https://bootstrap.pypa.io/pip/3.3/get-pip.py' -OutFile $GetPipFile
-    Invoke-Expression -Command "python $GetPipFile"
-    $pip = 1
-}
-if ($pip) {
-    if ([int]$Semver[0] -eq 3 -and [int]$Semver[1] -lt 5) {
+if ([int]$Semver[0] -eq 3 -and [int]$Semver[1] -lt 5) {
+    if ([int]$Semver[1] -gt 2) {
+        $VersionNumber = $Semver[0]
+        $Major = $Semver[1]
+        $GetPipFile = New-TemporaryFile
+        Invoke-WebRequest -Uri "https://bootstrap.pypa.io/pip/$VersionNumber.$Major/get-pip.py" -OutFile $GetPipFile
+        Invoke-Expression -Command "python $GetPipFile"
         Invoke-Expression -Command "python -m pip install typing"
+        if ([int]$Semver[1] -eq 4) {
+            Invoke-Expression -Command "python -m pip install --upgrade wheel setuptools"
+        }
     }
+} else {
+    Invoke-Expression -Command "python -m ensurepip"
+    Invoke-Expression -Command "python -m pip install --upgrade pip"
     Invoke-Expression -Command "python -m pip install --upgrade wheel setuptools"
 }
